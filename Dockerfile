dd018010701aa0ed14dd991c14e7e39592eb3eec
guox <guox@goodrain.com>
2019-08-18 01:54:03 +0800

    update all

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM centos:7
-RUN yum install -y http://www.pgpool.net/yum/rpms/4.0/redhat/rhel-7-x86_64/pgpool-II-release-4.0-1.noarch.rpm && \
-    yum makecache fast && \
-    yum install -y pgpool-II-pg96 pgpool-II-pg96-extensions && \
-    yum clean all && \
-    rm -rf /var/cache/yum
+FROM postdock/pgpool:latest-pgpool36-postgres10
 
-ADD start.sh /start.sh
-ENTRYPOINT ["/start.sh"]
+RUN apt-get update && \
+    apt-get install bindutils && \
+    apt-get clean && \
+    rm -rf /var/cache/apt
+
+ADD entrypoint.sh /usr/local/bin/pgpool/entrypoint.sh
+
+
+ENTRYPOINT ["/usr/local/bin/pgpool/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+
+export CONFIG_FILE='/usr/local/etc/pgpool.conf'
+export PCP_FILE='/usr/local/etc/pcp.conf'
+export HBA_FILE='/usr/local/etc/pool_hba.conf'
+export POOL_PASSWD_FILE='/usr/local/etc/pool_passwd'
+
+
+echo '>>> STARTING SSH (if required)...'
+sshd_start
+
+echo '>>> CONFIGING BACKEND HOSTS...'
+
+j=0
+unset IPS
+declare -a IPS
+for ips in $(nslookup ${DEPEND_SERVICE%:*} | grep Address | sed '1d' | awk '{print $2}')
+do
+IPS[$j]=$ips
+j=$j+1
+done
+
+
+BECKEND_NUMS=$(nslookup ${DEPEND_SERVICE%:*} | grep Address | sed '1d' | awk '{print $2}' | wc -l)
+
+for i in $(seq 0 `expr ${BECKEND_NUMS} - 1`)
+do
+IPS[$i]=$i\:${IPS[$i]}\:${POSTGRES_PORT}\:\1\:${PGDATA}\:ALLOW_TO_FAILOVER
+done
+
+
+BACKENDS=\"$(echo ${IPS[@]} | tr " " ",")\"
+export BACKENDS
+
+/var/pgpool_configs/pgpool.conf
+
+echo '>>> TURNING PGPOOL...'
+/usr/local/bin/pgpool/pgpool_setup.sh
+
+echo '>>> STARTING PGPOOL...'
+gosu postgres /usr/local/bin/pgpool/pgpool_start.sh
